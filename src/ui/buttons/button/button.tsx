b0809a9c327d946e8f18f7db7ba56ccc951aa368
Karelin Michail <r1no90@yandex.ru>
2023-09-05 23:52:32 +0300

    Добавлен элемент Button

--- src/ui/buttons/button/button.tsx
+++ src/ui/buttons/button/button.tsx
@@ -4,4 +4,48 @@
 
 // https://trello.com/c/ZimEto8x/7-buttons-contsructor
 
-export {};
+import React, { FC } from 'react';
+import cn from 'classnames';
+
+import PlusIcon from '../../../images/icon/24x24/screen navigation/plus.svg';
+
+import stylesButton from './button.module.scss';
+
+interface IButton {
+  size?: 'medium' | 'large';
+  color?: 'blue' | 'green' | 'grey';
+  buttonHtmlType?: 'button' | 'submit' | 'reset';
+  onClick?: VoidFunction;
+  disabled?: boolean;
+  children?: React.ReactNode;
+}
+
+const Button: FC<IButton> = ({
+  size = 'medium',
+  color = 'green',
+  buttonHtmlType = 'button',
+  onClick,
+  disabled,
+  children,
+}) => {
+  const mainCn = cn(
+    stylesButton.button,
+    stylesButton[size],
+    stylesButton[color],
+    children ? stylesButton.default : stylesButton.circle
+  );
+
+  return (
+    <button
+      className={mainCn}
+      onClick={onClick}
+      // eslint-disable-next-line react/button-has-type
+      type={buttonHtmlType}
+      disabled={disabled}
+    >
+      {children || <img src={PlusIcon} alt="PlusIcon" />}
+    </button>
+  );
+};
+
+export default Button;
