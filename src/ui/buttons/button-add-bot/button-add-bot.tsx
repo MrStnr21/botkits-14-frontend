e96e5b88a5fc887953d658722becbdee21a489f7
Karelin Michail <r1no90@yandex.ru>
2023-09-07 06:49:23 +0300

    new: Добавлен элемент ButtonAddBot.

--- src/ui/buttons/button-add-bot/button-add-bot.tsx
+++ src/ui/buttons/button-add-bot/button-add-bot.tsx
@@ -2,4 +2,40 @@
 // https://trello.com/c/CAyJT0Nk/6-buttons-common-2
 // 3 столбец в макете
 
-export {};
+import { FC } from 'react';
+import { ReactSVG } from 'react-svg';
+
+import PlusIcon from '../../../images/icon/24x24/screen navigation/plus.svg';
+
+import stylesButtonAddBot from './button-add-bot.module.scss';
+
+interface IButtonAddBot {
+  buttonHtmlType?: 'button' | 'submit' | 'reset';
+  onClick?: VoidFunction;
+  disabled?: boolean;
+  children?: React.ReactNode;
+}
+
+const ButtonAddBot: FC<IButtonAddBot> = ({
+  buttonHtmlType = 'button',
+  onClick,
+  disabled,
+  children,
+}) => {
+  return (
+    <button
+      className={stylesButtonAddBot.button}
+      onClick={onClick}
+      // eslint-disable-next-line react/button-has-type
+      type={buttonHtmlType}
+      disabled={disabled}
+    >
+      <div className={stylesButtonAddBot.container}>
+        <ReactSVG className={stylesButtonAddBot.icon} src={PlusIcon} />
+        <p className={stylesButtonAddBot.text}>{children}</p>
+      </div>
+    </button>
+  );
+};
+
+export default ButtonAddBot;
