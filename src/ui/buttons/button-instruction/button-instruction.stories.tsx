f95df5d7c4038df230d7ce2eb6c1670e511e3eef
Ruslan Akhmadiev <ahmadievrr@gmail.com>
2023-09-08 02:35:58 +0300

    new: кнопка инструкции и её история готовы

---
+++ src/ui/buttons/button-instruction/button-instruction.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryFn } from '@storybook/react';
+import ButtonInstruction, { IButtonInstruction } from './button-instruction';
+
+export default {
+  title: 'UI/Buttons/Button-Instruction',
+  component: ButtonInstruction,
+  argTypes: {
+    hover: {
+      description: 'Изменение кнопки при наведении',
+    },
+    disabled: {
+      type: 'boolean',
+      description: 'Вариант активности кнопки',
+      defaultValue: false,
+      options: [false, true],
+      control: {
+        type: 'radio',
+      },
+    },
+    onClick: {
+      action: 'clicked',
+      description: 'Переход на url с инструкцией',
+    },
+  },
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} as Meta<IButtonInstruction>;
+
+/* prettier-ignore */
+const Template: StoryFn<IButtonInstruction> = (args) => <ButtonInstruction {...args} />;
+
+export const Button = {
+  args: {
+    disabled: false,
+  },
+  render: Template,
+};
--- src/ui/buttons/button-instruction/button-instruction.tsx
+++ src/ui/buttons/button-instruction/button-instruction.tsx
@@ -3,30 +3,53 @@
 // копки во втором ряду, первый столбец с default до disabled
 
 import { FC } from 'react';
+import { ReactSVG } from 'react-svg';
 import stylesButtonInstruction from './button-instruction.module.scss';
 import FaqIcon from '../../../images/icon/add bot/tutorial.svg';
 
-interface IButtonInstruction {
+export interface IButtonInstruction {
   url: string;
+  disabled: boolean;
 }
 
-const ButtonInstruction: FC<IButtonInstruction> = ({ url }) => {
+const ButtonInstruction: FC<IButtonInstruction> = ({ url, disabled }) => {
   const onClick = () => {
     window.open(url, '_blank');
   };
 
+  const disabledClassnames = {
+    button:
+      disabled === false
+        ? stylesButtonInstruction.button
+        : stylesButtonInstruction.button__disabled,
+    image: disabled === false ? '1' : '.8',
+    text:
+      disabled === false
+        ? stylesButtonInstruction.text
+        : stylesButtonInstruction.text__disabled,
+  };
+
   return (
-    <div onClick={onClick} className={stylesButtonInstruction.button}>
+    <div onClick={onClick} className={disabledClassnames.button}>
       <div className={stylesButtonInstruction.image_box}>
-        <img
+        <ReactSVG
           src={FaqIcon}
-          alt="FAQ"
-          className={stylesButtonInstruction.image}
+          desc="FAQ"
+          beforeInjection={(svg) => {
+            svg.setAttribute(
+              'style',
+              `fill-opacity: ${disabledClassnames.image}`
+            );
+          }}
         />
       </div>
-      <p className={stylesButtonInstruction.text}>Пошаговая инструкция</p>
+      <p className={disabledClassnames.text}>Пошаговая инструкция</p>
     </div>
   );
 };
 
 export default ButtonInstruction;
+/* <img
+          src={FaqIcon}
+          alt="FAQ"
+        /> */
