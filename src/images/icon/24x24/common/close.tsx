0d9377fb0aad24ba3b62159d123786ffe6679eba
Katerina Bychikhina <orodiavoletto@yandex.ru>
2023-09-07 13:18:24 +0300

    добавит ui видео и его историю

---
+++ src/images/icon/24x24/common/close.tsx
@@ -0,0 +1,24 @@
+export default function CloseIcon(fill: string) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill={fill}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M19 5L5 19"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5 5L19 19"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
