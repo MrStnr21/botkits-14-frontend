5fc727ec58ad0a0507575ec9b7c37972c59d2424
Ruslan Akhmadiev <ahmadievrr@gmail.com>
2023-09-15 23:34:45 +0300

    new: меню с галочками и его история

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,35 +1,13 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import stylesApp from './app.module.scss';
-import MenuTime from '../../ui/menus/menu-time/menu-time';
 
 const App: FC = (): JSX.Element => {
-  const [isActive, setIsActive] = useState<boolean>(false);
-  const [value, setValue] = useState<string>('0:0');
-
-  const save = (str: string) => {
-    setValue(str);
-  };
-
-  const clear = () => {
-    setValue('0:0');
-  };
-
   return (
     <div className={stylesApp.app}>
       <h1>Hi!</h1>
-      <button
-        type="button"
-        className={stylesApp.button}
-        onClick={() => setIsActive(!isActive)}
-      >
-        {value}
+      <button type="button" className={stylesApp.button}>
+        click!
       </button>
-      <MenuTime
-        isActive={isActive}
-        top={250}
-        saveFunction={save}
-        clearFunction={clear}
-      />
     </div>
   );
 };
