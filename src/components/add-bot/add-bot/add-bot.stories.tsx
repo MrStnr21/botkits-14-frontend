ddcc96d535d109a7928422b908ba6e60d9c9873b
Kirill <ki.karpachev@gmail.com>
2023-09-16 01:37:15 +0300

    add: component/add-bot/add-bot, fix: ui/buttons/button-add-social(добавил extraClass)

---
+++ src/components/add-bot/add-bot/add-bot.stories.tsx
@@ -0,0 +1,26 @@
+import type { StoryObj } from '@storybook/react';
+import AddBot from './add-bot';
+
+const meta = {
+  title: 'COMPONENTS/AddBot/AddBot',
+  component: AddBot,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const AddBotDefault: Story = {
+  args: {
+    onClick: () => console.log(1),
+  },
+};
+
+export const AddBotSelectedFacebook: Story = {
+  args: {
+    onClick: () => console.log(1),
+    bot: {
+      name: 'Facebook',
+    },
+  },
+};
