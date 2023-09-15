11ff52d91ccd0666fea847ec185fbe570810c506
Ruslan Akhmadiev <ahmadievrr@gmail.com>
2023-09-15 19:45:26 +0300

    new: меню с выбором времени и его история

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,18 @@
 import { FC, useState } from 'react';
-
 import stylesApp from './app.module.scss';
+import MenuTime from '../../ui/menus/menu-time/menu-time';
 
 const App: FC = (): JSX.Element => {
-  const [active, setAcitve] = useState<boolean>(false);
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [value, setValue] = useState<string>('0:0');
+
+  const save = (str: string) => {
+    setValue(str);
+  };
+
+  const clear = () => {
+    setValue('0:0');
+  };
 
   return (
     <div className={stylesApp.app}>
@@ -11,10 +20,16 @@ const App: FC = (): JSX.Element => {
       <button
         type="button"
         className={stylesApp.button}
-        onClick={() => setAcitve(!active)}
+        onClick={() => setIsActive(!isActive)}
       >
-        Click!
+        {value}
       </button>
+      <MenuTime
+        isActive={isActive}
+        top={250}
+        saveFunction={save}
+        clearFunction={clear}
+      />
     </div>
   );
 };
