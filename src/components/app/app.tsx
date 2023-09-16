2022396afd73946a2bbd90ebcec1a2ac5c55cc23
Yaroslav Potrapelyuk <MrStnr21@yandex.ru>
2023-09-16 19:14:03 +0300

    refactor: убрать лишнее из App

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,7 @@
 import { FC } from 'react';
 import stylesApp from './app.module.scss';
 
-import Templates from '../dashbord/bots-templates/bots-templates';
-import AddBotPage from '../../pages/add-bot-page/add-bot-page';
+// import AddBotPage from '../../pages/add-bot-page/add-bot-page';
 
 const App: FC = (): JSX.Element => {
   return (
@@ -11,9 +10,7 @@ const App: FC = (): JSX.Element => {
       <button type="button" className={stylesApp.button}>
         click!
       </button>
-      <Templates />
-
-      <AddBotPage />
+      {/* <AddBotPage /> */}
     </div>
   );
 };
