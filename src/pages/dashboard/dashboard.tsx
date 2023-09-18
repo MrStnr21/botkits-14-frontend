4d97aafefb01fd5d634c52c8bf339fe70e6eb4ca
Kirill <ki.karpachev@gmail.com>
2023-09-18 17:58:52 +0300

    add: добавил получение ботов из стора и бэка

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,6 +1,6 @@
-import { FC } from 'react';
-// import { useAppDispatch, useAppSelector } from '../../services/hooks/hooks';
-// import { getBotsAction } from '../../services/actions/bots/getBot';
+import { FC, useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../services/hooks/hooks';
+import { getBotsAction } from '../../services/actions/bots/getBot';
 
 import stylesDashboard from './dashboard.module.scss';
 
@@ -9,16 +9,17 @@ import Templates from '../../components/dashbord/bots-templates/bots-templates';
 import MyBots from '../../components/dashbord/my-bots/my-bots';
 
 const Dashboard: FC = (): JSX.Element => {
-  // ToDo: получение ботов с сервера
-  // const token: any = useAppSelector(
-  //   (store) => store.signup.user?.accounts[0].credentials.accessToken || store.signin.user?.accounts[0].credentials.accessToken
-  // );
+  const token: any = useAppSelector(
+    (store) =>
+      store.signup.user?.accounts[0].credentials.accessToken ||
+      store.signin.user?.accounts[0].credentials.accessToken
+  );
 
-  // const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-  // useEffect(() => {
-  //   dispatch(getBotsAction(token));
-  // }, []);
+  useEffect(() => {
+    dispatch(getBotsAction(token));
+  }, []);
   return (
     <div className={stylesDashboard.dashboard}>
       <Templates />
