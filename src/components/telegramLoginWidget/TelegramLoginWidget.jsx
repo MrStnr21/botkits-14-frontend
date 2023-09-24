fa14f0545afa6cd83320996c8614214b88b8b1ed
Evgeny Rusakov <shoomec@yandex.ru>
2023-09-24 19:52:41 +0500

    prettier

--- src/components/telegramLoginWidget/TelegramLoginWidget.jsx
+++ src/components/telegramLoginWidget/TelegramLoginWidget.jsx
@@ -3,12 +3,12 @@ import React, { useEffect } from 'react';
 const TelegramLoginWidget = () => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "https://telegram.org/js/telegram-widget.js?22";
+    script.src = 'https://telegram.org/js/telegram-widget.js?22';
     script.async = true;
-    script.setAttribute('data-telegram-login', "botKits_coral_bot");
-    script.setAttribute('data-size', "large");
-    script.setAttribute('data-auth-url', "https://botkits.nomoreparties.co/telegram");
-    script.setAttribute('data-request-access', "write");
+    script.setAttribute('data-telegram-login', 'botKits_coral_bot');
+    script.setAttribute('data-size', 'large');
+    script.setAttribute('data-auth-url', 'https://botkits.nomoreparties.co/telegram');
+    script.setAttribute('data-request-access', 'write');
     document.body.appendChild(script);
 
     // При удалении компонента, удалите скрипт
@@ -17,7 +17,7 @@ const TelegramLoginWidget = () => {
     }
   }, []);
 
-  return <div id="telegram-login-widget"></div>;
-}
+  return <div id='telegram-login-widget'></div>;
+};
 
 export default TelegramLoginWidget;
