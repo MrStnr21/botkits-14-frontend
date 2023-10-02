5cc73d989c09e124162f45e65ba0a2dc1f0efb69
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-10-02 23:27:09 +0300

    feat: Добавить заголовок страницы

--- src/pages/subscription/subscription.tsx
+++ src/pages/subscription/subscription.tsx
@@ -3,7 +3,11 @@ import { FC } from 'react';
 import stylesSubscription from './subscription.module.scss';
 
 const Subscription: FC = (): JSX.Element => {
-  return <div className={stylesSubscription.title}>Subscription</div>;
+  return (
+    <div className={stylesSubscription.container}>
+      <h2 className={stylesSubscription.title}>Подписка и платежи</h2>
+    </div>
+  );
 };
 
 export default Subscription;
