d4a4954c3530a26c79673071530f641a3458a0c5
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2023-10-03 10:07:17 +0300

    отрисовать кнопку

--- src/pages/partnership/partnership.tsx
+++ src/pages/partnership/partnership.tsx
@@ -1,12 +1,21 @@
 import { FC, useState } from 'react';
+import { ReactSVG } from 'react-svg';
 
 import { StyledEngineProvider } from '@mui/material';
+import cn from 'classnames';
 import stylesPartnership from './partnership.module.scss';
 import ReferralsTable from '../../components/tables/referrals-table/referrals-table';
 import PaymentsTable from '../../components/tables/payments-table/payments-table';
 
+import chevron from '../../images/icon/20x20/chevron/down.svg';
+
 const Partnership: FC = (): JSX.Element => {
   const [isTableVisible, setTableVisible] = useState(false);
+
+  const chevronClassName = cn(
+    stylesPartnership.chevron,
+    isTableVisible && stylesPartnership.chevron_active
+  );
 
   const toggleSecondTable = () => {
     setTableVisible(!isTableVisible);
@@ -24,8 +33,12 @@ const Partnership: FC = (): JSX.Element => {
         <div className={stylesPartnership.paymentsTableContainer}>
           <div className={stylesPartnership.headContainer}>
             <h2 className={stylesPartnership.heading}>Выплаты</h2>
-            <button onClick={toggleSecondTable} type="button">
-              Кнопка
+            <button
+              type="button"
+              onClick={toggleSecondTable}
+              className={chevronClassName}
+            >
+              <ReactSVG src={chevron} />
             </button>
           </div>
           {isTableVisible && <PaymentsTable />}
