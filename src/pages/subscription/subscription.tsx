72366c66531f235ded2ba8a44473b5999a873acb
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-10-04 02:13:54 +0300

    feat: добавить функционал открытия того или иного попапа, в зависимости от события нажатия на кнопку

--- src/pages/subscription/subscription.tsx
+++ src/pages/subscription/subscription.tsx
@@ -1,10 +1,28 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Button from '../../ui/buttons/button/button';
 import Typography from '../../ui/typography/typography';
 
 import stylesSubscription from './subscription.module.scss';
+import ModalPopup from '../../components/popups/modal-popup/modal-popup';
+import ActivatePromoCodePopup from '../../components/popups/activate-promo-code-popup/activate-promo-code-popup';
+import useModal from '../../services/hooks/use-modal';
+import PaymentPopup from '../../components/popups/activate-subscription-popup/payment-popup';
 
 const Subscription: FC = (): JSX.Element => {
+  const { isModalOpen, closeModal, openModal } = useModal();
+  const [popupType, setPopupType] = useState<
+    'activatePromoCode' | 'subscription' | null
+  >(null);
+  const handleActivateSubscription = (type: 'subscription') => {
+    openModal();
+    setPopupType(type);
+  };
+
+  const handleActivatePromoCode = (type: 'activatePromoCode') => {
+    openModal();
+    setPopupType(type);
+  };
+
   return (
     <div className={stylesSubscription.container}>
       <h2 className={stylesSubscription.title}>Подписка и платежи</h2>
@@ -15,7 +33,11 @@ const Subscription: FC = (): JSX.Element => {
             <h4 className={stylesSubscription.subscription__status}>Статус</h4>
             <p className={stylesSubscription.subscription__info}>Инфо</p>
             <div className={stylesSubscription.subscription__button}>
-              <Button variant="default" color="green">
+              <Button
+                variant="default"
+                color="green"
+                onClick={() => handleActivateSubscription('subscription')}
+              >
                 Активировать подписку
               </Button>
             </div>
@@ -27,13 +49,25 @@ const Subscription: FC = (): JSX.Element => {
                 {`${0}₽`}
               </h3>
             </div>
-            <Button variant="default" color="grey">
+            <Button
+              variant="default"
+              color="grey"
+              onClick={() => handleActivatePromoCode('activatePromoCode')}
+            >
               Активировать промокод
             </Button>
           </div>
         </div>
         <div className={stylesSubscription.payment}>payment</div>
       </div>
+      {isModalOpen && (
+        <ModalPopup onClick={closeModal}>
+          <div className={stylesSubscription.modal}>
+            {popupType === 'activatePromoCode' && <ActivatePromoCodePopup />}
+            {popupType === 'subscription' && <PaymentPopup />}
+          </div>
+        </ModalPopup>
+      )}
     </div>
   );
 };
