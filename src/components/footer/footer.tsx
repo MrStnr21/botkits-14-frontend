b8779f886b001186dea86dc547cfda0d2f11d8f3
Ruslan Akhmadiev <ahmadievrr@gmail.com>
2023-10-05 01:55:32 +0300

    fix: сделана динамическая дата

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,9 +3,11 @@ import { FC } from 'react';
 import stylesFooter from './footer.module.scss';
 
 const Footer: FC = (): JSX.Element => {
+  const year = new Date().getFullYear();
+
   return (
     <footer className={stylesFooter.footer}>
-      <p className={stylesFooter.text}>&copy; 2023 BotKits</p>
+      <p className={stylesFooter.text}>&copy; {year} BotKits</p>
     </footer>
   );
 };
