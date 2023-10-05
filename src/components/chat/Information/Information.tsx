3b37d95a52c58c15958c314be84523caf2dab7d1
NataliyaNikulshina <victory515@yandex.ru>
2023-10-05 23:36:32 +0300

    refactor: Добавить кнопки и DropDownList

--- src/components/chat/Information/Information.tsx
+++ src/components/chat/Information/Information.tsx
@@ -1,6 +1,8 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import avatar from '../../../images/avatar/circled/bigAvatar.svg';
 import stylesInformation from './Information.module.scss';
+import DropDownList from '../DropDownList/DropDownList';
+import ChevronIcon from '../../icons/Chevron/ChevronIcon';
 
 interface IInformation {
   image?: string;
@@ -11,17 +13,42 @@ const Information: FC<IInformation> = ({
   image,
   name = 'Сергей Надеин',
 }): JSX.Element => {
+  const [isDisabled, setIsDisabled] = useState(true);
+
   return (
     <div className={stylesInformation.container}>
+      <div className={stylesInformation.chevron}>
+        <ChevronIcon color="#FFFFFF" position="right" />
+      </div>
       <img src={image || avatar} alt="Аватар" />
       <p className={stylesInformation.title}>{name}</p>
       <p className={stylesInformation.text}>Пользователь</p>
-      <nav>
-        <ul>
-          <li>Информация</li>
-          <li>Файлы</li>
-        </ul>
+      <nav className={stylesInformation.nav}>
+        <button
+          type="button"
+          className={stylesInformation.button}
+          disabled={isDisabled}
+          onClick={() => {
+            setIsDisabled(!isDisabled);
+          }}
+        >
+          Информация
+        </button>
+        <button
+          type="button"
+          className={stylesInformation.button}
+          disabled={!isDisabled}
+          onClick={() => {
+            setIsDisabled(!isDisabled);
+          }}
+        >
+          Файлы
+        </button>
       </nav>
+      <div className={stylesInformation.wrapper}>
+        <DropDownList caption="Информация о пользователе" />
+        <DropDownList caption="История действий" />
+      </div>
     </div>
   );
 };
