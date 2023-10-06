a4dfc1abaff90514de6a1f5a04d0c3b4f7fc4afe
NataliyaNikulshina <victory515@yandex.ru>
2023-10-06 16:23:07 +0300

    feat: Добавить компонент "Окно поиска в диалогах"

---
+++ src/components/chat/InputDialogsues/InputDialogsues.stories.tsx
@@ -0,0 +1,12 @@
+import type { StoryObj } from '@storybook/react';
+import InputDialogsues from './InputDialogsues';
+
+const meta = {
+  title: 'COMPONENTS/Chat/InputDialogsues',
+  component: InputDialogsues,
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const InputDialogsuesDefault: Story = {};
