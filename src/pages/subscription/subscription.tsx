e77373644f6006359808c983eee70ee4f359e6ce
DIGDES\Buzunov.K <Buzunov.K@digdes.com>
2023-10-06 18:59:30 +0300

    feat: Добавить кнопку с выпадающим меню

--- src/pages/subscription/subscription.tsx
+++ src/pages/subscription/subscription.tsx
@@ -1,11 +1,42 @@
-import { FC } from 'react';
+/* eslint-disable spaced-comment */
+import { FC, useState, MouseEvent } from 'react';
+import { ReactSVG } from 'react-svg';
 import cn from 'classnames';
 import Button from '../../ui/buttons/button/button';
 import Typography from '../../ui/typography/typography';
+import MenuSimple from '../../ui/menus/menu-simple/menu-simple';
 import { rows, PaymentTable } from './payment-table/payment-table';
+import icon from '../../images/icon/20x20/chevron/down.svg';
 import style from './subscription.module.scss';
 
 const Subscription: FC = (): JSX.Element => {
+  const buttonList = ['Все', 'Списания', 'Поступления'];
+  const [activeList, setActiveList] = useState(false);
+  const [buttonName, setButtonName] = useState('Все');
+  const [rowList, setRowList] = useState(rows);
+
+  const click = () => {
+    setActiveList(!activeList);
+  };
+
+  const filteredRows = (filterName: string): void => {
+    setRowList(() => {
+      if (filterName === 'Все') {
+        return rows;
+      }
+      return rows.filter((row) => row.operation === filterName);
+    });
+  };
+
+  const setActiveFilter = (e: MouseEvent<HTMLDivElement>) => {
+    //TODO Исправить типизацию
+    //@ts-ignore
+    setButtonName(e.target.textContent);
+    //@ts-ignore
+    filteredRows(e.target.textContent);
+    setActiveList(!activeList);
+  };
+
   return (
     <div className={style.container}>
       <Typography tag="h2">Подписка и платежи</Typography>
@@ -40,9 +71,24 @@ const Subscription: FC = (): JSX.Element => {
         <div className={style.payment}>
           <div className={style.payment__header}>
             <Typography tag="h4">История платежей</Typography>
-            <Typography tag="p">All</Typography>
+            <div className={style.dropdown}>
+              <button
+                type="button"
+                onClick={click}
+                className={style.dropdown__button}
+              >
+                {buttonName}
+                <ReactSVG src={icon} style={{ height: '20px' }} />
+              </button>
+              <MenuSimple
+                buttons={buttonList}
+                isActive={activeList}
+                onClick={setActiveFilter}
+                className={style.dropdown__list}
+              />
+            </div>
           </div>
-          <PaymentTable tableData={rows} />
+          <PaymentTable tableData={rowList} />
         </div>
       </div>
     </div>
