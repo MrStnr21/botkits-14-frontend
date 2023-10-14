f66e40b1054445a0f4bc19991c87e503bfe59400
Constantin Vanchurenkov <i@wacorasu.ru>
2023-10-15 00:25:48 +0300

    вынесение стилей выпадающего списка в отдульный модуль

--- src/ui/inputs/input-select/input-select.tsx
+++ src/ui/inputs/input-select/input-select.tsx
@@ -1,10 +1,9 @@
 import { FC, useState, useEffect } from 'react';
-
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { ThemeProvider, createTheme } from '@mui/material';
 import FormControl from '@mui/material/FormControl';
 import { ArrowLeftIcon } from '@mui/x-date-pickers';
 import MenuItem from '@mui/material/MenuItem';
+import { StyledEngineProvider } from '@mui/styled-engine-sc';
 
 import './input-select.module.scss';
 
@@ -37,21 +36,6 @@ const InputSelect: FC<IInputSelect> = ({
     }
   }, [resetSelect]);
 
-  const inputSelectTheme = createTheme({
-    typography: {
-      fontFamily: 'Open Sans, sans-serif',
-    },
-    components: {
-      MuiPaper: {
-        styleOverrides: {
-          root: {
-            boxShadow: '0px 6px 16px 0px rgba(21, 18, 51, 0.08)',
-          },
-        },
-      },
-    },
-  });
-
   const handleChange = (event: SelectChangeEvent<string | string[]>) => {
     const {
       target: { value },
@@ -61,7 +45,7 @@ const InputSelect: FC<IInputSelect> = ({
   };
 
   return (
-    <ThemeProvider theme={inputSelectTheme}>
+    <StyledEngineProvider injectFirst>
       <FormControl
         sx={{
           maxWidth: `${maxWidth}px`,
@@ -76,66 +60,15 @@ const InputSelect: FC<IInputSelect> = ({
           value={multiple ? inputValues : inputValues.join()}
           onChange={handleChange}
           IconComponent={ArrowLeftIcon}
-          sx={{
-            backgroundColor: '#F2F4F8',
-            fontSize: '13px',
-            fontWeight: 400,
-            fontStyle: 'normal',
-            lineHeight: '150%',
-            letterSpacing: '0.3px',
-            color: '#060C23',
-            textTransform: 'capitalize',
-            paddingTop: '10px',
-            paddingLeft: '8px',
-            paddingBottom: '10px',
-            maxHeight: '40px',
-            '.MuiOutlinedInput-notchedOutline': {
-              border: 'none',
-            },
-            '.MuiOutlinedInput-input': {
-              padding: 0,
-            },
-            '.MuiSelect-icon': {
-              maxWidth: '16px',
-              transform: 'rotate(-90deg)',
-            },
-          }}
         >
           {values.map(({ nameValue, value }) => (
-            <MenuItem
-              key={value}
-              value={value}
-              sx={{
-                '&:focus': {
-                  backgroundColor: '#F2F4F8',
-                },
-                '&.Mui-selected': {
-                  backgroundColor: '#F2F4F8',
-                },
-                '&.Mui-selected:hover': {
-                  backgroundColor: '#F2F4F8',
-                },
-                '&.MuiMenuItem-root': {
-                  fontSize: '13px',
-                  fontWeight: 400,
-                  fontStyle: 'normal',
-                  lineHeight: '150%',
-                  letterSpacing: '0.3px',
-                  color: '#060C23',
-                  textTransform: 'capitalize',
-                  maxHeight: '37px',
-                  paddingTop: '9px',
-                  paddingLeft: '16px',
-                  paddingBottom: '8px',
-                },
-              }}
-            >
+            <MenuItem key={value} value={value}>
               {nameValue}
             </MenuItem>
           ))}
         </Select>
       </FormControl>
-    </ThemeProvider>
+    </StyledEngineProvider>
   );
 };
 
