8bc4ad2f6ff651820dc2228fcf3c3e7dd37bcd9b
NataliyaNikulshina <victory515@yandex.ru>
2023-10-20 18:10:38 +0300

    reverts commit 3eb4de0

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -50,7 +50,7 @@ const App: FC = (): JSX.Element => {
         <Route
           path={routesUrl.homePage}
           element={
-            <ProtectedRoute notAuth>
+            <ProtectedRoute>
               <Layout />
             </ProtectedRoute>
           }
@@ -58,7 +58,7 @@ const App: FC = (): JSX.Element => {
           <Route
             path={routesUrl.homePage}
             element={
-              <ProtectedRoute notAuth>
+              <ProtectedRoute>
                 <Dashboard />
               </ProtectedRoute>
             }
@@ -82,7 +82,7 @@ const App: FC = (): JSX.Element => {
           <Route
             path={routesUrl.chat}
             element={
-              <ProtectedRoute notAuth>
+              <ProtectedRoute>
                 <Chat />
               </ProtectedRoute>
             }
--- src/pages/chat/chat.tsx
+++ src/pages/chat/chat.tsx
@@ -1,25 +1,9 @@
 import { FC } from 'react';
+
 import stylesChatDesktop from './chat.module.scss';
-import InputDialogsues from '../../components/chat/InputDialogsues/InputDialogsues';
-// import Information from '../../components/chat/Information/Information';
-// import MenuSimple from '../../ui/menus/menu-simple/menu-simple';
 
 const ChatDesktop: FC = (): JSX.Element => {
-  // const array = ['Сначала новые', 'Сначала старые', 'Неотвеченные'];
-  // const active = true;
-  return (
-    <div>
-      <div className={stylesChatDesktop.title}>ChatDesktop</div>
-      <InputDialogsues />
-      {/* <Information /> */}
-      {/* <MenuSimple
-        buttons={array}
-        isScroll={false}
-        isActive={active}
-        size="chat"
-      /> */}
-    </div>
-  );
+  return <div className={stylesChatDesktop.title}>ChatDesktop</div>;
 };
 
 export default ChatDesktop;
