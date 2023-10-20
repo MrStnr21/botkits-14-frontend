500d9c831cae6a2595b8ebe589a4cc3f64fb6a51
NataliyaNikulshina <victory515@yandex.ru>
2023-10-20 17:33:10 +0300

    refactor: Добавить меню фильтр в InputDialogsues

--- src/components/chat/InputDialogsues/InputDialogsues.tsx
+++ src/components/chat/InputDialogsues/InputDialogsues.tsx
@@ -1,20 +1,25 @@
-import { FC, ChangeEvent } from 'react';
+import { FC, ChangeEvent, useState } from 'react';
 import stylesInput from './InputDialogsues.module.scss';
 import FilterIcon from '../../icons/Filter/FilterIcon';
+import SearchFilters from '../SearchFilters/SearchFilters';
 
 interface IInputDialogsues {
   placeholder?: string;
   value?: string;
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
-  onClickFilter?: () => void;
 }
 
 const InputDialogsues: FC<IInputDialogsues> = ({
   value,
   placeholder = 'Поиск...',
   onChange,
-  onClickFilter,
 }): JSX.Element => {
+  const [isOpenFilters, setIsOpenFilters] = useState(false);
+
+  const toggleIsOpenFilters = () => {
+    setIsOpenFilters(!isOpenFilters);
+  };
+
   return (
     <div className={stylesInput.message}>
       <input
@@ -27,10 +32,11 @@ const InputDialogsues: FC<IInputDialogsues> = ({
       <button
         className={stylesInput.button}
         type="button"
-        onClick={onClickFilter}
+        onClick={toggleIsOpenFilters}
       >
         <FilterIcon />
       </button>
+      <SearchFilters active={isOpenFilters} />
     </div>
   );
 };
