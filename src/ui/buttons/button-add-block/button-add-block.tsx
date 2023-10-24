b42992afcfb59f6e4cdf5a8813839d25f7423328
Vadim Savinov <vasaykh@yandex.ru>
2023-10-24 21:17:04 +0500

    ref: Typography ButtonAddBlock

--- src/ui/buttons/button-add-block/button-add-block.tsx
+++ src/ui/buttons/button-add-block/button-add-block.tsx
@@ -1,8 +1,7 @@
 import { FC } from 'react';
-
 import stylesButtonAddBlock from './button-add-block.module.scss';
-
 import messageIcon from '../../../images/icon/24x24/add block/message-square.svg';
+import Typography from '../../typography/typography';
 
 export interface IButtonAddBlock {
   icon: string;
@@ -25,7 +24,9 @@ const ButtonAddBlock: FC<IButtonAddBlock> = ({
       disabled={disabled}
     >
       <img src={icon} className={stylesButtonAddBlock.icon} alt="иконка" />
-      <p className={stylesButtonAddBlock.name}>{name}</p>
+      <Typography tag="p" className={stylesButtonAddBlock.name}>
+        {name}
+      </Typography>
     </button>
   );
 };
