3252c5fb0988e88307a66aa4286344c0ef7d0e76
Vadim Savinov <vasaykh@yandex.ru>
2023-10-29 20:16:26 +0500

    feat: SendingCoordinatesBlock

---
+++ src/components/builder/blocks/sending-coordinates-block/sending-coordinates-block.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import SendingCoordinatesBlock from './sending-coordinates-block';
+
+const meta: Meta<typeof SendingCoordinatesBlock> = {
+  component: SendingCoordinatesBlock,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SendingCoordinatesBlock>;
+
+export const Block: Story = {
+  args: {
+    nameSetter: () => {},
+  },
+};
