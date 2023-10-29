2db6714eab29452bc656554c3aabfb0c30ac8442
Vadim Savinov <107421589+vasaykh2@users.noreply.github.com>
2023-10-29 20:37:48 +0500

    Update bot-builder.tsx

--- src/pages/bot-builder/bot-builder.tsx
+++ src/pages/bot-builder/bot-builder.tsx
@@ -1,17 +1,11 @@
 import { FC } from 'react';
 
 import stylesBotBuilder from './bot-builder.module.scss';
-import TriggerBlock from '../../components/builder/triggerBlock/triggerBlock';
-import HeaderBlock from '../../components/builder/blocks/header-block/header-block';
 
 const BotBuilder: FC = (): JSX.Element => {
   return (
     <div className={stylesBotBuilder.title}>
-      BotBuilder      
-      <div className={stylesBotBuilder.test}>
-        <HeaderBlock />
-      </div>
-      <TriggerBlock />
+      BotBuilder
     </div>
   );
 };
