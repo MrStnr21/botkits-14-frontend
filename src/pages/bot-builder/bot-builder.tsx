0c6b1991cde368fd408dd7a2b1189406dc92cdeb
Vadim Savinov <107421589+vasaykh2@users.noreply.github.com>
2023-10-29 20:35:04 +0500

    Update bot-builder.tsx

--- src/pages/bot-builder/bot-builder.tsx
+++ src/pages/bot-builder/bot-builder.tsx
@@ -2,16 +2,12 @@ import { FC } from 'react';
 
 import stylesBotBuilder from './bot-builder.module.scss';
 import TriggerBlock from '../../components/builder/triggerBlock/triggerBlock';
-import TelegramPayment from '../../components/builder/telegramPayment/telegramPayment';
 
 const BotBuilder: FC = (): JSX.Element => {
   return (
     <div className={stylesBotBuilder.title}>
       BotBuilder
-      <div className={stylesBotBuilder.test}>       
-      </div>
       <TriggerBlock />
-      <TelegramPayment />
     </div>
   );
 };
