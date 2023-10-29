c7339244f76c06805223c624e493d9e32d676888
Vadim Savinov <107421589+vasaykh2@users.noreply.github.com>
2023-10-29 20:29:11 +0500

    Update bot-builder.tsx

--- src/pages/bot-builder/bot-builder.tsx
+++ src/pages/bot-builder/bot-builder.tsx
@@ -2,16 +2,12 @@ import { FC } from 'react';
 
 import stylesBotBuilder from './bot-builder.module.scss';
 import TriggerBlock from '../../components/builder/triggerBlock/triggerBlock';
-// import SavingToCrmBlock from '../../components/builder/blocks/saving-to-crm-block/saving-to-crm-block';
 import HeaderBlock from '../../components/builder/blocks/header-block/header-block';
 
 const BotBuilder: FC = (): JSX.Element => {
   return (
     <div className={stylesBotBuilder.title}>
-      BotBuilder
-      {/* <div className={stylesBotBuilder.test}>
-        <SavingToCrmBlock />
-      </div> */}
+      BotBuilder      
       <div className={stylesBotBuilder.test}>
         <HeaderBlock />
       </div>
