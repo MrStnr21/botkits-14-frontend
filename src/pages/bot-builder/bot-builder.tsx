108c5a9d425b8547f940f69764d00840cb2d70c1
Polina Komarova <polina-komarova-95@mail.ru>
2023-10-30 13:43:16 +0300

    stories added

--- src/pages/bot-builder/bot-builder.tsx
+++ src/pages/bot-builder/bot-builder.tsx
@@ -2,18 +2,14 @@ import { FC } from 'react';
 
 import stylesBotBuilder from './bot-builder.module.scss';
 import TriggerBlock from '../../components/builder/triggerBlock/triggerBlock';
-// import VariableBlockNode from '../../components/builder/blocks/variable/variable';
-// import SavingToCrmBlock from '../../components/builder/blocks/saving-to-crm-block/saving-to-crm-block';
-import ApiBlockNode from '../../components/builder/blocks/api/api';
+import SavingToCrmBlock from '../../components/builder/blocks/saving-to-crm-block/saving-to-crm-block';
 
 const BotBuilder: FC = (): JSX.Element => {
   return (
     <div className={stylesBotBuilder.title}>
       BotBuilder
       <div className={stylesBotBuilder.test}>
-        {/* <SavingToCrmBlock /> */}
-        <ApiBlockNode />
-        {/* <VariableBlockNode /> */}
+        <SavingToCrmBlock />
       </div>
       <TriggerBlock />
     </div>
