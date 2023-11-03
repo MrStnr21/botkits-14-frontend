8316a216c88076b849ec3764dcb2ae88cea0a8bf
Vadim Savinov <vasaykh@yandex.ru>
2023-11-03 14:00:00 +0500

    fix: ButtonS

--- src/components/builder/flow/initial-nodes.tsx
+++ src/components/builder/flow/initial-nodes.tsx
@@ -10,26 +10,26 @@ import {
   // OnConnect,
 } from 'reactflow';
 
-import ButtonSFlow from '../blocks/buttons/buttons-flow';
+// import ButtonSFlow from '../blocks/buttons/buttons-flow';
 // import InlineButton from '../blocks/message-block/button-inline/button-inline';
 // import PanelInline from '../blocks/message-block/panel-inline/panel-inline';
-
 // import styles from './layoutFlow.module.scss';
 import 'reactflow/dist/style.css';
+import ButtonS from '../blocks/buttons/buttons';
 import MessageBlock from '../blocks/message-block/message-block';
 
 export const nodeTypes: NodeTypes = {
   // inlineButton: InlineButton,
   message: MessageBlock,
   // panelInline: PanelInline,
-  buttonSFlow: ButtonSFlow,
+  button: ButtonS,
 };
 
 export const initialNodes: Node[] = [
   {
     id: 'node-1',
-    type: 'buttonSFlow',
-    data: { type: 'test' },
+    type: 'button',
+    data: { type: 'start' },
     position: { x: 0, y: 0 },
   },
   {
