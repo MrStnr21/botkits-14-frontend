0c283ed591f1f284a213f363f67e0a3d5de4498c
seliveren <selivanyulia@mail.ru>
2023-11-05 12:19:11 +0300

    feat: добавить файл с моковыми данными

--- src/components/chat/dialogs/dialogs.tsx
+++ src/components/chat/dialogs/dialogs.tsx
@@ -1,74 +1,11 @@
 import { FC } from 'react';
 import styles from './dialogs.module.scss';
 import Dialog from '../dialog/dialog';
-import InputDialogsues from '../InputDialogsues/InputDialogsues';
+import InputDialogsues from '../InputDialogsues/InputDialogues';
 import Typography from '../../../ui/typography/typography';
+import { testData } from '../../../utils/mockData';
 
 const Dialogs: FC = (): JSX.Element => {
-  const testData = [
-    {
-      user: {
-        id: '1',
-        name: 'Юлия',
-        status: 'online',
-      },
-      lastMessageAt: '2023-11-03 18:30:22',
-      textMessage: 'Привет! Ну что там с этим вашим проектом??',
-      status: 'unread',
-    },
-    {
-      user: {
-        id: '1',
-        name: 'Юлия',
-        status: 'online',
-      },
-      lastMessageAt: '2022-11-03 18:30:22',
-      textMessage: 'Ghbdtn!',
-      status: 'unread',
-    },
-    {
-      user: {
-        id: '2',
-        name: 'Галя',
-        status: 'offline',
-      },
-      lastMessageAt: '2023-09-27 13:15:00',
-      textMessage:
-        'Привет! Ну что-то делаем, что тебе ещё сказать - даже и не знаю',
-      status: 'read',
-    },
-    {
-      user: {
-        id: '2',
-        name: 'Галя',
-        status: 'offline',
-      },
-      lastMessageAt: '2023-09-26 13:15:00',
-      textMessage: 'Привет! Нуууууууууу',
-      status: 'unread',
-    },
-    {
-      user: {
-        id: '3',
-        name: 'Алексей',
-        status: 'offline',
-      },
-      lastMessageAt: '2022-05-22 12:17:19',
-      textMessage: 'Привет! Ну что там там там там там там там',
-      status: 'read',
-    },
-    {
-      user: {
-        id: '4',
-        name: 'Карина',
-        status: 'offline',
-      },
-      lastMessageAt: '2022-10-27 18:47:33',
-      textMessage: 'Привет! Всё отлично!!',
-      status: 'read',
-    },
-  ];
-
   const count: number[] = [];
 
   testData.forEach((el) => {
