30a291b66f65cd8aa4099527a5d11aa2d9076cdd
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2023-11-08 18:41:12 +0300

    initial commit

--- src/pages/chat/chat.tsx
+++ src/pages/chat/chat.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react';
 import ChatDialogue from '../../components/chat-dialogue/chat-dialogue';
 import stylesChatDesktop from './chat.module.scss';
+import Dialogs from '../../components/chat/dialogs/dialogs';
+import Information from '../../components/chat/Information/Information';
 
 const ChatDesktop: FC = (): JSX.Element => {
   return (
     <div className={stylesChatDesktop.layout}>
+      <Dialogs />
       <ChatDialogue />
+      <Information />
     </div>
   );
 };
