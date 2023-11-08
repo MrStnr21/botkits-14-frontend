24096068937c2d083889a3922e39643b256fdce2
Vadim Savinov <vasaykh@yandex.ru>
2023-11-08 19:17:26 +0500

    fix: delete heder-block

--- src/components/builder/blocks/header-block/header-block.stories.tsx
+++
@@ -1,8 +0,0 @@
-import HeaderBlock from './header-block';
-
-export default {
-  title: 'COMPONENTS/Builder/blocks/headerBlock',
-  component: HeaderBlock,
-};
-
-export const Default = () => <HeaderBlock />;
