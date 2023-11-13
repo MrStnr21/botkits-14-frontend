9afd480db781cc91635e57edbf5eae93c1e0678d
Nastasia <lisitsinan@gmail.com>
2023-11-13 18:29:53 +0200

    feat: добавить панель декорирования с тултипами, адаптивность

--- src/ui/text-field/text-field.tsx
+++ src/ui/text-field/text-field.tsx
@@ -2,28 +2,32 @@ import { FC, useState } from 'react';
 
 import EmojiPicker from 'emoji-picker-react';
 
-import stylesTextField from './text-field.module.scss';
+import styles from './text-field.module.scss';
 
-import { ReactComponent as Bracket } from '../../images/icon/24x24/constructor/bracket.svg';
-import { ReactComponent as EmojiIcon } from '../../images/icon/24x24/constructor/emoji.svg';
-import Typography from '../typography/typography';
-import useOutsideClick from '../../utils/hooks/useOutsideClick';
+import bracketIcon from '../../images/icon/24x24/constructor/bracket.svg';
+import emojiIcon from '../../images/icon/24x24/constructor/emoji.svg';
+import MenuTextEditor from '../menus/menu-text-editor/menu-text-editor';
 
 interface ITextField {
   maxTextLength?: number;
 }
 
 const TextField: FC<ITextField> = ({ maxTextLength = 4096 }) => {
-  const [showEmojis, setShowEmojis] = useState(false);
+  const [emojis, toggleEmojis] = useState(false);
+  const [textMenu, toggleTextMenu] = useState(false);
+
+  const onBracketClick = () => {
+    toggleTextMenu(!textMenu);
+  };
+
+  const onEmojiClick = () => {
+    toggleEmojis(!emojis);
+  };
 
   const [text, setText] = useState('');
 
-  const refPicker = useOutsideClick(() => {
-    setShowEmojis(false);
-  });
-
   return (
-    <div className={stylesTextField.textarea}>
+    <div className={styles.textarea}>
       <textarea
         name="textarea"
         id="textarea"
@@ -32,42 +36,40 @@ const TextField: FC<ITextField> = ({ maxTextLength = 4096 }) => {
         value={text}
         onChange={(e) => setText(e.target.value)}
         draggable={false}
-        className={stylesTextField.textarea__input}
+        className={styles.textarea__input}
       />
-      <span className={stylesTextField.textarea__outline} />
+      <span className={styles.textarea__outline} />
 
-      <div className={stylesTextField.textarea__footer}>
-        <Typography
-          tag="p"
-          fontFamily="secondary"
-          className={stylesTextField.textarea__counter}
-        >
+      <div className={styles.textarea__footer}>
+        <span className={styles.textarea__counter}>
           {text.length}/{maxTextLength}
-        </Typography>
-        <Bracket
-          className={stylesTextField.textarea__icon}
-          onClick={() => console.log('Что-то не происходит')}
-        />
-        {showEmojis && (
-          <div
-            className={stylesTextField.textarea__emojiPicker}
-            ref={refPicker}
-          >
-            (
+        </span>
+        <button
+          className={styles.textarea__font}
+          onClick={onBracketClick}
+          type="button"
+        >
+          <img className={styles.icon} src={bracketIcon} alt="Шрифт" />
+        </button>
+        <MenuTextEditor isActive={textMenu} top={-60} left={36} />
+        <button
+          className={styles.textarea__font}
+          onClick={onEmojiClick}
+          type="button"
+        >
+          <img className={styles.icon} src={emojiIcon} alt="Эмодзи" />
+        </button>
+        {emojis && (
+          <div className={styles.textarea__emojiPicker}>
             <EmojiPicker
-              width={300}
+              width={264}
               onEmojiClick={(e) => {
                 setText(`${text}${e.emoji}`);
-                setShowEmojis(false);
+                toggleEmojis(false);
               }}
             />
-            )
           </div>
         )}
-        <EmojiIcon
-          className={stylesTextField.textarea__icon}
-          onClick={() => setShowEmojis(!showEmojis)}
-        />
       </div>
     </div>
   );
