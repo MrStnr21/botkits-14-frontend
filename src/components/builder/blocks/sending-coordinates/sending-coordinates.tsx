d192cd95e31a113b89857cc802d0607ab193c2fb
KulQG <cool.egor-kot@yandex.ru>
2023-11-14 16:20:26 +0300

    first

--- src/components/builder/blocks/sending-coordinates/sending-coordinates.tsx
+++ src/components/builder/blocks/sending-coordinates/sending-coordinates.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useState } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
+import { useNodeId, useReactFlow } from 'reactflow';
 import styles from './sending-coordinates.module.scss';
 import ControlLayout from '../../control-layout/control-layout';
 import LabeledInput from '../../labeledInput/labeledInput';
@@ -12,6 +13,46 @@ const SendingCoordinatesBlock: FC<TBlockProps<TCoordinateBlock>> = ({
   data,
 }) => {
   const [name, setName] = useState(data.name);
+  const [coordinates, setCoordinates] = useState({
+    latitude: '',
+    longitude: '',
+  });
+  const { setNodes, getNodes } = useReactFlow();
+  const nodeId = useNodeId();
+
+  const save = () => {
+    const newNodes = getNodes().map((node) => {
+      if (node.id === nodeId) {
+        return {
+          ...node,
+          data: {
+            ...data,
+            coordinates: [coordinates.longitude, coordinates.latitude],
+          },
+        };
+      }
+      return node;
+    });
+
+    setNodes(newNodes);
+
+    console.log(
+      // eslint-disable-next-line array-callback-return, consistent-return
+      getNodes().map((node) => {
+        if (node.id === nodeId) return node;
+      })
+    );
+  };
+
+  const onChangeNodeInput = (
+    e: ChangeEvent<HTMLInputElement>,
+    type: 'latitude' | 'longitude'
+  ) => {
+    const newCoordinates = { ...coordinates, [type]: e.target.value };
+    setCoordinates(newCoordinates);
+
+    save();
+  };
 
   return (
     <ControlLayout
@@ -27,10 +68,12 @@ const SendingCoordinatesBlock: FC<TBlockProps<TCoordinateBlock>> = ({
             <Input
               minLength={0}
               type="number"
-              onChange={() => {}}
+              onChange={(e) => {
+                onChangeNodeInput(e, 'longitude');
+              }}
               styled="bot-builder-default"
               placeholder="Введите параметр"
-              value={String(data.coordinates[0] || '')}
+              value={String(data.coordinates[0] || coordinates.longitude)}
             />
           </LabeledInput>
         </div>
@@ -39,10 +82,12 @@ const SendingCoordinatesBlock: FC<TBlockProps<TCoordinateBlock>> = ({
             <Input
               minLength={0}
               type="number"
-              onChange={() => {}}
+              onChange={(e) => {
+                onChangeNodeInput(e, 'latitude');
+              }}
               styled="bot-builder-default"
               placeholder="Введите параметр"
-              value={String(data.coordinates[1] || '')}
+              value={String(data.coordinates[1] || coordinates.latitude)}
             />
           </LabeledInput>
         </div>
