2958b425de5b714498cf6cbc43827300e2c071fe
Vadim Savinov <vasaykh@yandex.ru>
2023-11-19 20:55:58 +0500

    feat: addPhoto to TelegramPayment

---
+++ src/components/builder/blocks/telegram-payment/aad-photo/aad-photo.tsx
@@ -0,0 +1,44 @@
+import { FC, useRef } from 'react';
+
+import ConstructorAddButton from '../../../../../ui/buttons/constructor-add-button/constructor-add-button';
+
+import { BUTTON_NAME } from '../../../../../utils/constants';
+
+type TAadPhotodProps = {
+  addFile?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const AadPhoto: FC<TAadPhotodProps> = ({ addFile }) => {
+  const ref = useRef<null | HTMLInputElement>(null);
+
+  const onClickAddPhoto = () => {
+    if (ref.current) {
+      ref.current.click();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        ref={ref}
+        type="file"
+        id={BUTTON_NAME.IMAGE}
+        name={BUTTON_NAME.IMAGE}
+        accept=".jpg, .png, .gif"
+        onChange={addFile}
+        hidden
+      />
+      <label htmlFor={BUTTON_NAME.IMAGE}>
+        <ConstructorAddButton
+          maxWidth="100%"
+          icon="photo"
+          onClick={onClickAddPhoto}
+        >
+          Добавить фото
+        </ConstructorAddButton>
+      </label>
+    </div>
+  );
+};
+
+export default AadPhoto;
--- src/components/builder/blocks/telegram-payment/telegram-payment.tsx
+++ src/components/builder/blocks/telegram-payment/telegram-payment.tsx
@@ -1,20 +1,19 @@
-import { FC, useState, ChangeEvent, useRef, useEffect } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { useReactFlow, useNodeId } from 'reactflow';
 import ControlLayout from '../../control-layout/control-layout';
 import styles from './telegram-payment.module.scss';
 import LabeledInput from '../../labeledInput/labeledInput';
 import Input from '../../../../ui/inputs/input/input';
-import ConstructorAddButton from '../../../../ui/buttons/constructor-add-button/constructor-add-button';
 import MenumenuSelectFlow from '../../../../ui/menus/menu-select-flow/menu-select-flow';
 
 import {
   TBlockProps,
   TTelegramPayBlock,
 } from '../../../../services/types/builder';
-import { BUTTON_NAME } from '../../../../utils/constants';
 import { currencyAvailable, messagesSuccessful } from '../../utils/data';
 import { setFlowData } from '../../utils';
-// import VideoCard from '../../../video-card/video-card';
+import File from '../message-block/file/file';
+import AadPhoto from './aad-photo/aad-photo';
 
 const TelegramPayment: FC<TBlockProps<TTelegramPayBlock>> = ({ data }) => {
   const [active, setActive] = useState(data.onSuccess !== '');
@@ -54,9 +53,6 @@ const TelegramPayment: FC<TBlockProps<TTelegramPayBlock>> = ({ data }) => {
       value: name,
     });
 
-  const i = 2;
-  const a = getNodes()[i].data.name + getNodes()[i].data.currency;
-
   const setOnSuccess = (value: string) =>
     setFlowDataButton({
       selectors: ['onSuccess'],
@@ -67,22 +63,36 @@ const TelegramPayment: FC<TBlockProps<TTelegramPayBlock>> = ({ data }) => {
     selectors: ['goodsName'],
   });
 
+  const setDescription = setFlowData({
+    selectors: ['description'],
+  });
+
   const setPayment = setFlowData({
     selectors: ['payment'],
   });
 
-  const [description, setDescription] = useState(data.description || '');
-
+  const setProviderToken = setFlowData({
+    selectors: ['providerToken'],
+  });
   const placeholder = 'Введите название';
   const buttonsCurrency = currencyAvailable.map((item) => item.nameValue);
   const buttonsOnSuccess = messagesSuccessful.map((item) => item.nameValue);
 
-  const ref = useRef<null | HTMLInputElement>(null);
-
-  const onClickAddPhoto = () => {
-    if (ref.current) {
-      ref.current.click();
-    }
+  const addFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNodes(
+      getNodes().map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            data: {
+              ...item.data,
+              image: e.target.files && e.target.files[0],
+            },
+          };
+        }
+        return item;
+      })
+    );
   };
 
   return (
@@ -94,42 +104,19 @@ const TelegramPayment: FC<TBlockProps<TTelegramPayBlock>> = ({ data }) => {
             value={data.goodsName || ''}
             onChange={setGoodsName}
             styled="bot-builder-default"
+            minLength={0}
           />
         </LabeledInput>
-        <div>
-          <input
-            ref={ref}
-            type="file"
-            id={BUTTON_NAME.IMAGE}
-            name={BUTTON_NAME.IMAGE}
-            accept=".jpg, .png, .gif"
-            hidden
-          />
-          <label htmlFor={BUTTON_NAME.IMAGE}>
-            <ConstructorAddButton
-              maxWidth="100%"
-              icon="photo"
-              onClick={onClickAddPhoto}
-            >
-              Добавить фото
-            </ConstructorAddButton>
-          </label>
-          {/* <VideoCard
-            contentType="image"
-            title="title"
-            prewiew={URL.createObjectURL(file)}
-            src={URL.createObjectURL(file)}
-          /> */}
-        </div>
+        <File key={data.name + data.image} data={data.image} />
+        <AadPhoto addFile={addFile} />
 
         <LabeledInput title="Описание">
           <Input
             placeholder={placeholder}
-            value={description}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => {
-              setDescription(e.target.value);
-            }}
+            value={data.description || ''}
+            onChange={setDescription}
             styled="bot-builder-default"
+            minLength={0}
           />
         </LabeledInput>
         <LabeledInput title="Сумма к оплате">
@@ -139,9 +126,10 @@ const TelegramPayment: FC<TBlockProps<TTelegramPayBlock>> = ({ data }) => {
               value={data.payment ? String(data.payment) : ''}
               onChange={setPayment}
               styled="bot-builder-default"
+              minLength={0}
+              type="number"
             />
             <div className={styles.wrapperCurrency}>
-              {`${a} sssddd`}
               <MenumenuSelectFlow
                 buttons={buttonsCurrency}
                 nameMenu={data.currency || buttonsCurrency[0]}
@@ -155,8 +143,9 @@ const TelegramPayment: FC<TBlockProps<TTelegramPayBlock>> = ({ data }) => {
           <Input
             placeholder={placeholder}
             value={data.providerToken}
-            onChange={() => {}}
+            onChange={setProviderToken}
             styled="bot-builder-default"
+            minLength={0}
           />
         </LabeledInput>
         <LabeledInput title="После успешной оплаты вернуть">
