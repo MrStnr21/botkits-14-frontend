761df44141b9c702f5101614e62a65ed5aaa4b22
K0nstant1nS <K1kollov@gmail.com>
2023-12-01 11:42:55 +0300

    feat: scss variables export

---
+++ src/stylesheets/_variables.scss.d.ts
@@ -0,0 +1,33 @@
+interface ISCSSVariables {
+  [key: string]: string;
+  grey1: string;
+  grey2: string;
+  grey3: string;
+  grey4: string;
+  blueDisabled: string;
+  offWhite: string;
+  primaryPersianBlueDefault: string;
+  primaryPersianBluePress: string;
+  primaryPersianBlueHover: string;
+  primaryPersianBlueFocus: string;
+  primaryOxfordBlueDefault: string;
+  primaryMediumSpringGreenDefault: string;
+  primaryMediumSpringGreenHover: string;
+  primaryMediumSpringGreenDisabled: string;
+  secondaryCulturedHover: string;
+  secondaryCulturedDefault: string;
+  secondaryCulturedPress: string;
+  secondaryCulturedFocus: string;
+  secondaryXiketicDefault: string;
+  secondaryXiketicPress: string;
+  errorDefault: string;
+  successDefault: string;
+  exampleButtonColorRed: string;
+  exampleButtonColorGreen: string;
+  exampleButtonColorBlue: string;
+  flowBackgroundColor: string;
+  switchingWidth: string;
+}
+
+export const variables: ISCSSVariables;
+export default variables;
