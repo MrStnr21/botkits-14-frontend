99dfabb6fe8f2abb2979ae1bafd1b68c323c186b
Nastasia <lisitsinan@gmail.com>
2023-12-11 15:40:03 +0200

    feat: смена цвета через css

--- src/ui/icon/icon.tsx
+++ src/ui/icon/icon.tsx
@@ -8,17 +8,13 @@ export interface IIcon {
   icon: IconName;
   /** Размер в пикселях */
   size: number;
-  /** Цвет иконки в формате HEX. Если не задан, то будет использован оригинальный цвет иконки */
-  color?: string;
   /** Дополнительная стилизация иконки */
   extraClass?: string;
 }
 
-const Icon: FC<IIcon> = ({ icon, size, color, extraClass }) => {
+const Icon: FC<IIcon> = ({ icon, size, extraClass }) => {
   const filterId = useId();
   const [iconSrc, setIconSrc] = useState<string | null>(null);
-  const [isHovered, setIsHovered] = useState<boolean>(false);
-  const [iconColor, setIconColor] = useState<string | undefined>(color);
   const iconRef = useRef<SVGSVGElement | null>(null);
 
   useEffect(() => {
@@ -35,36 +31,19 @@ const Icon: FC<IIcon> = ({ icon, size, color, extraClass }) => {
     fetchIcon();
   }, [icon]);
 
-  const applyHoverColor = () => {
-    if (iconRef.current && isHovered) {
-      const hoverColor = getComputedStyle(iconRef.current).getPropertyValue(
-        'color'
-      );
-      setIconColor(hoverColor);
-    } else setIconColor(color);
-  };
-
-  useEffect(applyHoverColor, [isHovered]);
-
   return (
-    <svg
-      width={size}
-      height={size}
-      className={`${extraClass} ${isHovered ? 'hovered' : ''}`}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      ref={iconRef}
-    >
-      {iconColor && (
+    <svg width={size} height={size} className={extraClass} ref={iconRef}>
+      {extraClass && (
         <filter id={filterId}>
-          <feFlood in="SourceGraphic" floodColor={iconColor} />
+          <feFlood in="SourceGraphic" floodColor="var(--flood-color)" />
           <feComposite in2="SourceAlpha" operator="in" />
         </filter>
       )}
+
       {iconSrc && (
         <image
           href={iconSrc}
-          filter={color ? `url(#${filterId})` : undefined}
+          filter={`url(#${filterId})`}
           width="100%"
           height="100%"
           x="0"
