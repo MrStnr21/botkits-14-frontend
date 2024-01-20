0bf61f5576eda78d2d1150752279aff2f6778102
Vadim Savinov <vasaykh@yandex.ru>
2024-01-20 23:06:53 +0500

    fix: storybook для ControlLayout

--- src/components/builder/control-layout/control-layout.stories.tsx
+++ src/components/builder/control-layout/control-layout.stories.tsx
@@ -1,15 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { ReactFlowProvider } from 'reactflow';
-// import { Provider } from 'react-redux';
-// eslint-disable-next-line import/no-extraneous-dependencies
-// import { withRouter } from 'storybook-addon-react-router-v6';
-// import store from '../../../services/store';
 import ControlLayout from './control-layout';
 
 const meta: Meta<typeof ControlLayout> = {
   title: 'COMPONENTS/Builder/control-layout',
   component: ControlLayout,
-  // decorators: [withRouter],
 };
 
 const styleContainer = {
@@ -29,10 +24,8 @@ export default meta;
 
 type Story = StoryObj<typeof ControlLayout>;
 
-// Вариант с рендером сразу двух блоков
 export const LayoutFilled: Story = {
   render: () => (
-    // <Provider store={store}>
     <ReactFlowProvider>
       <div style={{ display: 'flex', flexDirection: 'column', gap: '40px' }}>
         <ControlLayout type="Тип блока">
@@ -44,6 +37,5 @@ export const LayoutFilled: Story = {
         </ControlLayout>
       </div>
     </ReactFlowProvider>
-    // </Provider>
   ),
 };
