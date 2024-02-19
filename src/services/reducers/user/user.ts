2b8cb75ce4e6ce5c42bd2934ee4fbafeba635956
K0nstant1nS <K1kollov@gmail.com>
2024-02-19 11:10:17 +0300

    fix: state error fix

--- src/services/reducers/user/user.ts
+++ src/services/reducers/user/user.ts
@@ -1,4 +1,5 @@
-// eslint-disable-next-line import/no-cycle
+/* eslint-disable import/no-cycle */
+import { Reducer } from 'redux';
 import {
   GET_USER_INFO_REQUEST,
   GET_USER_INFO_SUCCESS,
@@ -28,11 +29,11 @@ const getUserInfoInitialState: TGetUserInfoState = {
   getUserInfoError: false,
 };
 
-function getUserInfoReducer(
+const getUserInfoReducer: Reducer = (
   // eslint-disable-next-line @typescript-eslint/default-param-last
   state = getUserInfoInitialState,
   action: TGetUserInfoActions
-) {
+) => {
   switch (action.type) {
     // экшены авторизации
     case GET_USER_INFO_REQUEST: {
@@ -61,6 +62,6 @@ function getUserInfoReducer(
       return state;
     }
   }
-}
+};
 
 export { getUserInfoReducer };
