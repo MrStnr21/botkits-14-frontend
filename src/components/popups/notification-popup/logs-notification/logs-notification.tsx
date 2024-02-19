f72ef7b5374943a26503b918b7cd5715b3132c8e
Nastasia <lisitsinan@gmail.com>
2024-02-19 14:33:11 +0200

    feat: перенаправление на страницу, связанную с уведомлением

--- src/components/popups/notification-popup/logs-notification/logs-notification.tsx
+++ src/components/popups/notification-popup/logs-notification/logs-notification.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './logs-notification.module.scss';
 import getTimeAgo from '../../../../utils/getTimeAgo';
 import Icon from '../../../../ui/icon/icon';
@@ -9,7 +10,24 @@ type TLogsNotificationProps = {
   title: string;
   text?: string;
   date: string;
+  link?: string;
   remove: (id: string) => void;
+};
+
+type TWrapper = {
+  children: React.ReactNode;
+  link?: string;
+};
+
+const Wrapper: FC<TWrapper> = ({ link, children }) => {
+  if (link) {
+    return (
+      <Link to={link} className={styles.link}>
+        {children}
+      </Link>
+    );
+  }
+  return <div>{children}</div>;
 };
 
 const LogsNotification: FC<TLogsNotificationProps> = ({
@@ -18,20 +36,23 @@ const LogsNotification: FC<TLogsNotificationProps> = ({
   title,
   text,
   date,
+  link,
   remove,
 }) => {
   return (
     <article className={styles.container}>
-      <div className={styles.header}>
-        <Icon
-          icon="rocket"
-          isColored
-          extraClass={`${styles.icon} ${styles[status]}`}
-        />
-        <p className={styles.time}>{getTimeAgo(new Date(date))}</p>
-      </div>
-      <h4 className={styles.title}>{title}</h4>
-      {text && <p className={styles.text}>{text}</p>}
+      <Wrapper link={link}>
+        <div className={styles.header}>
+          <Icon
+            icon="rocket"
+            isColored
+            extraClass={`${styles.icon} ${styles[status]}`}
+          />
+          <p className={styles.time}>{getTimeAgo(new Date(date))}</p>
+        </div>
+        <h4 className={styles.title}>{title}</h4>
+        {text && <p className={styles.text}>{text}</p>}
+      </Wrapper>
       <button
         onClick={() => remove(_id)}
         className={styles.button}
