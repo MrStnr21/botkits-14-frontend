541bd13656c3b5c81c430fcacd11b7c6a6e5cafd
Nastasia <lisitsinan@gmail.com>
2024-03-11 17:50:18 +0200

    fix: типизация useForm

--- src/services/hooks/use-form.ts
+++ src/services/hooks/use-form.ts
@@ -1,11 +1,19 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
+
+export type TInputValue = { value: string | number; isValid?: boolean };
+export type TFormValues = Record<string, TInputValue>;
 
 /**
  * хук для добавления отслеживания состояния формы
  * @param inputValues объект с полями формата: `name: { value: string, isValid: boolean }`, где name это атрибут tag инпута
  */
-function useForm(inputValues: any) {
-  const [values, setValues] = useState(inputValues);
+function useForm<T extends TFormValues>(inputValues: T) {
+  const [values, setValues] = useState<T>(inputValues);
+  const [isFormValid, setIsFormValid] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsFormValid(Object.values(values).every((field) => field.isValid));
+  }, [values]);
 
   const handleChange = (
     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -13,11 +21,12 @@ function useForm(inputValues: any) {
     const { value, name } = event.target;
     setValues({
       ...values,
-      [name]: { value, valueValid: event.target.validity.valid },
+      [name]: { value, isValid: event.target.validity.valid },
     });
   };
   return {
     values,
+    isFormValid,
     handleChange,
     setValues,
   };
